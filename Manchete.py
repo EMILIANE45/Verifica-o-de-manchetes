8c89cf9a2f461e9e1fcd2a6a8ff9323ced7bf9cc
EMILIANE C <149191555+EMILIANE45@users.noreply.github.com>
2023-11-17 16:03:10 -0300

    Create Manchete.py

---
+++ Manchete.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+url='https://tribunademinas.com.br/noticias/cultura'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    
+    soup = BeautifulSoup(response.text,'html.parser')
+    
+    noticias = soup.find_all('div',class_='col-sm-12')
+
+    for noticia in noticias:
+        
+        manchete = noticia.find('h2')
+        
+        if manchete:
+            
+            print(manchete.text)
+else:
+    print("falha na requisição: status code", response.status_code)
